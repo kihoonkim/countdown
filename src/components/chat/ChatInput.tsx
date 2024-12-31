18fbc692f9b348657460084c228663c8c6f762f9
ki <koreakihoon@gmail.com>
2024-12-31 18:28:27 +0900

    isComposing

--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -52,7 +52,7 @@ function ChatInput({ name, onNameChanged, onEnter }: Props) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleOnKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key !== 'Enter') return
+    if (e.key !== 'Enter' && !e.nativeEvent.isComposing) return
     handleOnEnter()
   }
   const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
